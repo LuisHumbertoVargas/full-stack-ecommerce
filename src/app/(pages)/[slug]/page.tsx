f8015076668b5f0e7d75aa95e834ba30ae8d59c0
Luis <luish.vargas09@gmail.com>
2023-12-28 14:45:29 -0600

    Promotion section

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -1,3 +1,10 @@
+/**
+ * This file contains the code for rendering a page in a React application.
+ * It fetches data from an API and renders the page based on the fetched data.
+ * The page can be a home page or any other page specified by the slug parameter.
+ * If the page does not exist, it returns a 404 page.
+ * The code also includes functions for generating static parameters and metadata for the page.
+ */
 import React from 'react'
 import { Metadata } from 'next'
 import { draftMode } from 'next/headers'
@@ -8,25 +15,16 @@ import { staticHome } from '../../../payload/seed/home-static'
 import { fetchDoc } from '../../_api/fetchDoc'
 import { fetchDocs } from '../../_api/fetchDocs'
 import { Blocks } from '../../_components/Blocks'
+import Categories from '../../_components/Categories'
 import { Gutter } from '../../_components/Gutter'
 import { Hero } from '../../_components/Hero'
+import Promotion from '../../_components/Promotion'
 import { generateMeta } from '../../_utilities/generateMeta'
-
-// Payload Cloud caches all files through Cloudflare, so we don't need Next.js to cache them as well
-// This means that we can turn off Next.js data caching and instead rely solely on the Cloudflare CDN
-// To do this, we include the `no-cache` header on the fetch requests used to get the data for this page
-// But we also need to force Next.js to dynamically render this page on each request for preview mode to work
-// See https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
-// If you are not using Payload Cloud then this line can be removed, see `../../../README.md#cache`
-export const dynamic = 'force-dynamic'
-
-import Categories from '../../_components/Categories'
 
 import classes from './index.module.scss'
 
 export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
-
   let page: Page | null = null
   let categories: Category[] | null = null
 
@@ -36,18 +34,13 @@ export default async function Page({ params: { slug = 'home' } }) {
       slug,
       draft: isDraftMode,
     })
-
     categories = await fetchDocs<Category>('categories')
   } catch (error) {
-    // when deploying this template on Payload Cloud, this page needs to build before the APIs are live
-    // so swallow the error here and simply render the page with fallback data where necessary
-    // in production you may want to redirect to a 404  page or at least log the error somewhere
-    // console.error(error)
+    // Swallow the error and render the page with fallback data
+    // In production, you may want to redirect to a 404 page or log the error
   }
 
-  // if no `home` page exists, render a static one using dummy content
-  // you should delete this code once you have a home page in the CMS
-  // this is really only useful for those who are demoing this template
+  // Render a static home page if no 'home' page exists in the CMS
   if (!page && slug === 'home') {
     page = staticHome
   }
@@ -65,6 +58,7 @@ export default async function Page({ params: { slug = 'home' } }) {
           <Hero {...hero} />
           <Gutter className={classes.home}>
             <Categories categories={categories} />
+            <Promotion />
           </Gutter>
         </section>
       ) : (
@@ -91,7 +85,6 @@ export async function generateStaticParams() {
 
 export async function generateMetadata({ params: { slug = 'home' } }): Promise<Metadata> {
   const { isEnabled: isDraftMode } = draftMode()
-
   let page: Page | null = null
 
   try {
@@ -101,10 +94,9 @@ export async function generateMetadata({ params: { slug = 'home' } }): Promise<M
       draft: isDraftMode,
     })
   } catch (error) {
-    // don't throw an error if the fetch fails
-    // this is so that we can render a static home page for the demo
-    // when deploying this template on Payload Cloud, this page needs to build before the APIs are live
-    // in production you may want to redirect to a 404  page or at least log the error somewhere
+    // Don't throw an error if the fetch fails
+    // Render a static home page for the demo
+    // In production, you may want to redirect to a 404 page or log the error
   }
 
   if (!page && slug === 'home') {
